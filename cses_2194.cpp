3869e2b2bd1f245e7839491e4d5a41ad851fd1df
EzElephant <guu1016@gmail.com>
2023-04-10 23:53:15 +0800

    2023/01/10

--- cses_2194.cpp
+++ cses_2194.cpp
@@ -1,6 +1,7 @@
 #include <bits/extc++.h>
 using namespace std;
 #define double long long
+int mode = 0;
 struct PT
 {
     double x, y;
@@ -23,7 +24,12 @@ struct PT
     }
     bool operator<(const PT &b) const
     {
-        return x != b.x ? x < b.x : y < b.y; 
+        if (mode == 0)
+            return x != b.x ? x < b.x : y < b.y;
+        else if (mode == 1)
+            return y != b.y ? y < b.y : x < b.x;
+        else
+            return x + y != b.x + b.y ? x + y < b.x + b.y : x < b.x;
     }
     double dot(const PT &b) const
     {
@@ -38,96 +44,6 @@ struct PT
         return x * x + y * y;
     }
 };
-bool btw(const PT &p1, const PT &p2, const PT &p3)
-{ // 判斷p3是否在p1 p2之間(要先共線)
-    return (p1 - p3).dot(p2 - p3) <= 0;
-}
-bool collinearity(const PT &p1, const PT &p2, const PT &p3)
-{ // 判斷是否共線
-    return (p1 - p3).cross(p2 - p3) == 0;
-}
-bool pointOnSegment(const PT &p1, const PT &p2, const PT &p3)
-{
-    return collinearity(p1, p2, p3) && btw(p1, p2, p3);
-}
-double area(const vector<PT> &Polygon)
-{ // 多邊形面積
-    if (Polygon.size() <= 1)
-        return 0;
-    double ans = 0;
-    for (auto a = --Polygon.end(), b = Polygon.begin(); b != Polygon.end(); a = b++)
-        ans += a->cross(*b);
-    return ans / 2;
-}
-int pointInPolygon(const vector<PT> &Polygon, const PT &p)
-{
-    int ans = 0;
-    for (auto a = --Polygon.end(), b = Polygon.begin(); b != Polygon.end(); a = b++)
-    {
-        if (pointOnSegment(*a, *b, p))
-            return -1;
-        if ((a->y > p.y) != (b->y > p.y) && (p.x - b->x) < (a->x - b->x) * (p.y - b->y) / (a->y - b->y))
-        {
-            ans = !ans;
-        }
-    }
-    return ans;
-}
-int ori(const PT &p1, const PT &p2, const PT &p3)
-{
-    double a = (p2 - p1).cross(p3 - p1);
-    if (a == 0)
-        return 0;
-    return a > 0 ? 1 : -1;
-}
-bool seg_intersect(const PT &p1, const PT &p2, const PT &p3, const PT &p4)
-{ // 線段是否相交
-    int a123 = ori(p1, p2, p3);
-    int a124 = ori(p1, p2, p4);
-    int a341 = ori(p3, p4, p1);
-    int a342 = ori(p3, p4, p2);
-    if (a123 == 0 && a124 == 0)
-        return btw(p1, p2, p3) || btw(p1, p2, p4) || btw(p3, p4, p1) || btw(p3, p4, p2);
-    else if (a123 * a124 <= 0 && a341 * a342 <= 0)
-        return true;
-    return false;
-}
-PT intersect(const PT &p1, const PT &p2, const PT &p3, const PT &p4)
-{ // 找交點
-    double a123 = (p2 - p1).cross(p3 - p1);
-    double a124 = (p2 - p1).cross(p4 - p1);
-    return (p4 * a123 - p3 * a124) / (a123 - a124);
-}
-vector<PT> getConvexHull(vector<PT> Points)
-{ // 找凸包
-    sort(Points.begin(), Points.end());
-    vector<PT> ans;
-    int m = 0, t = 1;
-    auto addPoint = [&](const PT &p)
-    {
-        while (m > t && (ans[m - 1] - ans[m - 2]).cross(p - ans[m - 2]) <= 0)
-            ans.pop_back(), --m;
-        ans.emplace_back(p);
-        ++m;
-    };
-    for (size_t i = 0; i < Points.size(); ++i)
-        addPoint(Points[i]);
-    t = m;
-    for (int i = int(Points.size()) - 2; i >= 0; --i)
-        addPoint(Points[i]);
-    if (Points.size() > 1)
-        ans.pop_back();
-    return ans;
-}
-double solve(vector<PT> ConvexHull)
-{
-    int n = ConvexHull.size();
-    double result = 4e18 + 8787;
-    for (int i = 0; i < n; i++)
-        for (int j = i + 1; j < n; j++)
-            result = min(result, (ConvexHull[i] - ConvexHull[j]).abs2());
-    return result;
-}
 
 int main()
 {
@@ -135,6 +51,7 @@ int main()
     cin.tie(0);
 
     int n;
+    long long result = 8e18 + 87;
     cin >> n;
     vector<PT> points(n);
     for (int i = 0; i < n; i++)
@@ -143,7 +60,21 @@ int main()
         cin >> x >> y;
         points[i] = PT(x, y);
     }
-    cout << solve(getConvexHull(points)) << '\n';
+    sort(points.begin(), points.end());
+    for (int i = 0; i < n; i++)
+        for (int j = 1; (j * j < n || j < 487) && i + j < n; j++)
+            result = min(result, (points[i] - points[i + j]).abs2());
+    mode = 1;
+    sort(points.begin(), points.end());
+    for (int i = 0; i < n; i++)
+        for (int j = 1; (j * j < n || j < 487) && i + j < n; j++)
+            result = min(result, (points[i] - points[i + j]).abs2());
+    mode = 2;
+    sort(points.begin(), points.end());
+    for (int i = 0; i < n; i++)
+        for (int j = 1; (j * j < n || j < 487) && i + j < n; j++)
+            result = min(result, (points[i] - points[i + j]).abs2());
+    cout << result << '\n';
 
     return 0;
 }
